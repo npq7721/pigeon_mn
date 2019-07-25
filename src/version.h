7e178e2d00b5c115386ea338368291073d696255
Tri Nguyen <tri282@gmail.com>
2019-07-24 19:48:24 -0700

    fixed protocol version

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70215;
+static const int PROTOCOL_VERSION = 70015;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70213;
+static const int MIN_PEER_PROTO_VERSION = 70015;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -39,19 +39,19 @@ static const int NO_BLOOM_VERSION = 70201;
 static const int SENDHEADERS_VERSION = 70201;
 
 //! DIP0001 was activated in this version
-static const int DIP0001_PROTOCOL_VERSION = 70208;
+static const int DIP0001_PROTOCOL_VERSION = 70015;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 70209;
+static const int SHORT_IDS_BLOCKS_VERSION = 70015;
 
 //! introduction of DIP3/deterministic masternodes
-static const int DMN_PROTO_VERSION = 70213;
+static const int DMN_PROTO_VERSION = 70015;
 
 //! introduction of LLMQs
-static const int LLMQS_PROTO_VERSION = 70214;
+static const int LLMQS_PROTO_VERSION = 70015;
 
 //! introduction of SENDDSQUEUE
 //! TODO we can remove this in 0.15.0.0
-static const int SENDDSQUEUE_PROTO_VERSION = 70214;
+static const int SENDDSQUEUE_PROTO_VERSION = 70015;
 
 #endif // BITCOIN_VERSION_H
